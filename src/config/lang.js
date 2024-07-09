0068b47834e24f6f81dabb3fccb3de47ef42488d
LexTech <ryanvalentin288@gmail.com>
2024-07-09 18:03:00 +0100

    Help Yoshida

--- src/config/lang.js
+++ src/config/lang.js
@@ -1,5 +1,5 @@
 function getLinks() {
-    const LINKS = ["https://t.me/+_XOymY47tkdkODlk", "https://t.me/+1WojOmxBDfY5ODQ8", "https://t.me/+tmQGUmNs61FiNWM0", "https://t.me/+E1Sr6gkcDqtmZTRk"];
+    const LINKS = ["https://t.me/+_XOymY47tkdkODlk", "https://t.me/+XvquHoIihW9hZWJk", "https://t.me/+tmQGUmNs61FiNWM0", "https://t.me/+E1Sr6gkcDqtmZTRk"];
     return LINKS.reduce((prev, link) => prev + `👉 ${link}\n`, "");
 }
 
