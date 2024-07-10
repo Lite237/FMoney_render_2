d7033dec2056dcb0c4fba25b2bd76e0b6d42fb71
LexTech <ryanvalentin288@gmail.com>
2024-07-10 11:01:10 +0100

    Personnel link added

--- src/config/lang.js
+++ src/config/lang.js
@@ -1,5 +1,5 @@
 function getLinks() {
-    const LINKS = ["https://t.me/+_XOymY47tkdkODlk", "https://t.me/+BJrruHK8wDpkMzQ8", "https://t.me/+1WojOmxBDfY5ODQ8", "https://t.me/+E1Sr6gkcDqtmZTRk"];
+    const LINKS = ["https://t.me/+_XOymY47tkdkODlk", "https://t.me/+BJrruHK8wDpkMzQ8", "https://t.me/+HUZcS2O2uUhhYjc0", "https://t.me/+E1Sr6gkcDqtmZTRk"];
     return LINKS.reduce((prev, link) => prev + `👉 ${link}\n`, "");
 }
 
